0378c5dd7a8849ec4004e2020f683aaf34b20122
Yorick de Wid <yorick17@outlook.com>
2019-07-06 14:46:05 +0200

    Database service

--- Poller/Poller.Host/Extensions/ServiceCollectionRemotePublisherExtensions.cs
+++ Poller/Poller.Host/Extensions/ServiceCollectionRemotePublisherExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Poller.Publisher;
 
