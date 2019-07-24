a15ded7f7af48ada3c3c8a230b6331ee93993bc5
yorickdewid <yorick17@outlook.com>
2019-07-25 00:36:56 +0200

    Basic event bus impl.

--- Poller/Poller/Scheduler/Activator/EventActivator.cs
+++ Poller/Poller/Scheduler/Activator/EventActivator.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 namespace Poller.Scheduler.Activator
 {
     public class EventActivator : ActivatorBase
     {
+        private const string ServiceBusConnectionString = "<your_connection_string>";
+        private const string QueueName = "<your_queue_name>";
+        private readonly IQueueClient queueClient;
+
         public EventActivator(IServiceProvider serviceProvider, IOperationDelegate operation)
             : base(serviceProvider, operation)
         {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, new MessageHandlerOptions(null));
+        }
+
+        private async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            ExecuteProvider();
+
+            await Task.CompletedTask;
         }
 
         public override void Initialize(CancellationToken token)
