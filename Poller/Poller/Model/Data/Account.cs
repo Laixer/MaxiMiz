51a6fea1fe610fe8bd1b9dc038bb12ae6d89be55
Yorick de Wid <yorick17@outlook.com>
2019-07-29 14:31:39 +0200

    Database account model

---
+++ Poller/Poller/Model/Data/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Poller.Model.Data
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string SecondaryId { get; set; }
+        public string Publisher { get; set; }
+        public string Name { get; set; }
+        public string Currency { get; set; }
+    }
+}
