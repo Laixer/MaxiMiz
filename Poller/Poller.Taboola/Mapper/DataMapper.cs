82444328c27cfdf676e285114ba6f894a43999de
thomasbeckers <tabeckers95@gmail.com>
2019-08-11 16:50:45 +0200

    Created new structure for datamapper

--- Poller/Poller.Taboola/Mapper/DataMapper.cs
+++ Poller/Poller.Taboola/Mapper/DataMapper.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using AccountMaximiz = Maximiz.Model.Entity.Account;
+using AccountTaboola = Poller.Taboola.Model.Account;
+
 namespace Poller.Taboola.Mapper
 {
 
@@ -11,7 +14,7 @@ namespace Poller.Taboola.Mapper
     /// gap between our own models and those of the
     /// Taboola API.
     /// </summary>
-    public class DataMapper
+    class DataMapper
     {
 
         private IMapper _mapperAccount;
@@ -19,67 +22,24 @@ namespace Poller.Taboola.Mapper
         /// <summary>
         /// Constructor.
         /// </summary>
-        public DataMapper() => CreateMappings();
+        DataMapper() => CreateMappers();
 
         /// <summary>
         /// Creates our custom mapping configurations.
         /// </summary>
-        private void CreateMappings()
+        private void CreateMappers()
         {
-            CreateAccountMapping();
+            _mapperAccount = new ConfigAccount().CreateMapper();
+        }
+       
+        AccountMaximiz TaboolaToMaximiz(AccountTaboola account)
+        {
+            return _mapperAccount.Map<AccountTaboola, AccountMaximiz>(account);
         }
 
-        private void CreateAccountMapping()
+        AccountTaboola MaximizToTaboola(AccountMaximiz account)
         {
-            var configuration = new MapperConfiguration(cfg =>
-            {
-
-                // Custom conversion string to int
-                cfg.CreateMap<string, int>().ConvertUsing(s => Convert.ToInt32(s));
-
-                // Map for account
-                cfg.CreateMap<Taboola.Model.Account, Maximiz.Model.Entity.Account>()
-                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-                .ForMember(dest => dest.SecondaryId, opt => opt.MapFrom(src => src.AccountId))
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-                .ForMember(dest => dest.Publisher, opt => opt.MapFrom("Taboola"))
-                .ForMember(dest => dest.Currency, opt => opt.MapFrom(src => src.Currency))
-                .ForMember(dest => dest.SecondaryId, opt => opt.MapFrom(src => src.Name))
-
-                // Null substitution
-                .ForMember(destination => destination.Name,
-                opt => opt.NullSubstitute("Default name"))
-
-                // Custom mapping
-
-
-                // Also create reverse
-                .ReverseMap();
-            });
-
-            configuration.AssertConfigurationIsValid();
-            _mapperAccount = configuration.CreateMapper();
-        }
-
-        Maximiz.Model.Entity.Account TaboolaToMaximiz(Model.Account account)
-        {
-            return _mapperAccount.Map<Model.Account, Maximiz.Model.Entity.Account>(account);
-        }
-
-        void MaximizToTaboola()
-        {
-            //return _mapperAccount.
-        }
-
-        Maximiz.Model.Entity.Account TaboolaToMaximizHardCoded(Model.Account account)
-        {
-            var result = new Maximiz.Model.Entity.Account
-            {
-
-
-            };
-
-            return result;
+            return null;
         }
 
     }
