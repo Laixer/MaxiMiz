fce9f4fc4600628466284c552aa7bc3eccb8f9d4
thomasbeckers <tabeckers95@gmail.com>
2019-08-11 16:52:02 +0200

    Switched account and accountdetails for better readability

--- Poller/Poller.Taboola/Model/Account.cs
+++ Poller/Poller.Taboola/Model/Account.cs
@@ -8,43 +8,66 @@ using Poller.Helper;
 namespace Poller.Taboola.Model
 {
 
-    [DataContract]
-    internal class AccountDetails
-    {
-        [DataMember(Name = "partner_types")]
-        public string[] PartnerTypes { get; set; }
-
-        [DataMember(Name = "type")]
-        public string Type { get; set; }
-
-        [DataMember(Name = "campaign_types")]
-        public string[] CampaignTypes { get; set; }
-    }
-
+    /// <summary>
+    /// Represents a Taboola account.
+    /// </summary>
     [DataContract]
     internal class Account
     {
+        /// <summary>
+        /// Taboola internal id, being an integer.
+        /// TODO Is this correct?
+        /// </summary>
         [DataMember(Name = "id")]
         public string Id { get; set; }
 
+        /// <summary>
+        /// User created name.
+        /// </summary>
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// User created name, whitespaces replaced 
+        /// with underscores.
+        /// </summary>
         [DataMember(Name = "account_id")]
         public string AccountId { get; set; }
 
+        /// <summary>
+        /// List of partner types. Options:
+        /// - PUBLISHER
+        /// - ADVERTISER
+        /// TODO Is this correct?
+        /// </summary>
         [DataMember(Name = "partner_types")]
         public string[] PartnerTypes { get; set; }
 
+        /// <summary>
+        /// Account type.
+        /// TODO What does this mean?
+        /// </summary>
         [DataMember(Name = "type")]
         public string Type { get; set; }
 
+        /// <summary>
+        /// Currency string.
+        /// </summary>
         [DataMember(Name = "Currency")]
         public string Currency { get; set; }
 
+        /// <summary>
+        /// List of types of campaigns belonging to 
+        /// this account.
+        /// TODO What does this mean?
+        /// </summary>
         [DataMember(Name = "campaign_types")]
         public string[] CampaignTypes { get; set; }
 
+        /// <summary>
+        /// JSON string containing unused data which
+        /// we do have to store.
+        /// </summary>
         public string Details
         {
             // FUTURE: Improve
@@ -55,5 +78,28 @@ namespace Poller.Taboola.Model
                 CampaignTypes = CampaignTypes?.Select(s => s.ToLowerInvariant()).ToArray(),
             });
         }
+    }
+
+    /// <summary>
+    /// Represents the properties of our 
+    /// Taboola account which we do not
+    /// store explicitly in our own database.
+    /// </summary>
+    [DataContract]
+    internal class AccountDetails
+    {
+
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+
+        [DataMember(Name = "partner_types")]
+        public string[] PartnerTypes { get; set; }
+
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "campaign_types")]
+        public string[] CampaignTypes { get; set; }
+
     }
 }
