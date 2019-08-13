8f2381fed1f3e4637249878fa2f614211d2d9be7
tabeckers <tabeckers95@gmail.com>
2019-08-13 15:43:59 +0200

    Interface for our bidirectional mapper

---
+++ Poller/Poller.Taboola/Mapper/IMapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Poller.Taboola.Mapper
+{
+
+    /// <summary>
+    /// Interface for mapping objects from Taboola
+    /// to our Core.
+    /// </summary>
+    interface IMapper<A, B>
+    {
+
+        /// <summary>
+        /// Converter from A to B.
+        /// </summary>
+        /// <param name="from">B object</param>
+        /// <returns>A object</returns>
+        A Convert(B from);
+
+        /// <summary>
+        /// Converter from B to A.
+        /// </summary>
+        /// <param name="from">A object</param>
+        /// <returns>B object</returns>
+        B Convert(A from);
+
+    }
+}
