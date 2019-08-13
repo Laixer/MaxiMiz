cc5fc3fab70cf5e1f84abe57adb3f9fbf012ad77
tabeckers <tabeckers95@gmail.com>
2019-08-13 15:30:34 +0200

    AdItem now only has required properties. No functions included, no AdItemCoResult properties included.

--- Poller/Poller.Taboola/Model/AdItem.cs
+++ Poller/Poller.Taboola/Model/AdItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.Serialization;
 using Poller.Extensions;
 using Poller.Model.Data;
@@ -7,10 +8,8 @@ namespace Poller.Taboola.Model
 
     /// <summary>
     /// Mirrors the Taboola AdItem result we get
-    /// from the functions:
-    /// - TODO List these
-    /// 
-    /// This is coupled with <see cref="AdItemCoResult"/>.
+    /// from all ad item functions. This is coupled 
+    /// with <see cref="AdItemCoResult"/>.
     /// </summary>
     [DataContract]
     internal class AdItem
@@ -21,33 +20,12 @@ namespace Poller.Taboola.Model
         [DataMember(Name = "id")]
         public string Id { get; set; }
 
-        /// </summary>
-        [DataMember(Name = "item")]
-        public string _Id { set => Id = value; }
-
         /// <summary>
-        /// The id of the campaign this item belongs to.
+        /// Unique numeric ID of the items parent campaign
+        /// returned as a string.
         /// </summary>
         [DataMember(Name = "campaign_id")]
-        public long CampaignId { set => Campaign = value; }
-
-        /// <summary>
-        /// The id of the campaign this item belongs to.
-        /// </summary>
-        [DataMember(Name = "campaign")]
-        public long Campaign { get; set; }
-
-        /// <summary>
-        /// The name the campaign this item belongs to.
-        /// </summary>
-        [DataMember(Name = "campaign_name")]
-        public string CampaignName { get; set; }
-
-        /// <summary>
-        /// The url for the thumbnail of this item.
-        /// </summary>
-        [DataMember(Name = "thumbnail_url")]
-        public string Content { get; set; }
+        public string CampaignId { get; set; }
 
         /// <summary>
         /// The url where the item leads to.
@@ -61,50 +39,6 @@ namespace Poller.Taboola.Model
         [DataMember(Name = "title")]
         public string Title { get; set; }
 
-        public string TitleText { get => string.IsNullOrEmpty(Title) ? "INVALID" : Title; }
-
-        /// <summary>
-        /// The ad title.
-        /// </summary>
-        [DataMember(Name = "item_name")]
-        public string _Title { set => Title = value; }
-
-        /// <summary>
-        /// The amount of clicks on this item.
-        /// </summary>
-        [DataMember(Name = "clicks")]
-        public long Clicks { get; set; }
-
-        /// <summary>
-        /// The amount of impressions on this item.
-        /// </summary>
-        [DataMember(Name = "impressions")]
-        public long Impressions { get; set; }
-
-        /// <summary>
-        /// Cost per click.
-        /// </summary>
-        [DataMember(Name = "cpc")]
-        public decimal Cpc { get; set; }
-
-        /// <summary>
-        /// The amount spent on this item.
-        /// </summary>
-        [DataMember(Name = "spent")]
-        public decimal Spent { get; set; }
-
-        /// <summary>
-        /// The currency in which all monetary units are calculated.
-        /// </summary>
-        [DataMember(Name = "currency")]
-        public string Currency { get; set; }
-
-        /// <summary>
-        /// The amount of actions on the item.
-        /// </summary>
-        [DataMember(Name = "actions")]
-        public long Actions { get; set; }
-
         /// <summary>
         /// Whether the item is still active.
         /// </summary>
@@ -116,61 +50,13 @@ namespace Poller.Taboola.Model
         /// </summary>
         [DataMember(Name = "approval_state")]
         public ApprovalState ApprovalState { get; set; }
-        public string ApprovalStateText { get => ApprovalState.GetEnumMemberName(); }
+
 
         /// <summary>
-        /// Ad item status.
+        /// Campaign item status.
         /// </summary>
         [DataMember(Name = "status")]
-        public CampaignItemStatus CampaignItemStatus
-        {
-            get
-            {
-                switch (Status)
-                {
-                    case AdItemStatus.Running:
-                        return CampaignItemStatus.Running;
-                    case AdItemStatus.Paused:
-                        return CampaignItemStatus.Paused;
-                    case AdItemStatus.Stopped:
-                        return CampaignItemStatus.Stopped;
-                    case AdItemStatus.Pending:
-                        return CampaignItemStatus.PendingApproval;
-                    case AdItemStatus.Rejected:
-                        return CampaignItemStatus.Rejected;
-                    default:
-                        return CampaignItemStatus.PendingApproval;
-                }
-            }
+        public CampaignItemStatus CampaignItemStatus { get; set; }
 
-            set
-            {
-                switch (value)
-                {
-                    case CampaignItemStatus.Running:
-                        Status = AdItemStatus.Running;
-                        break;
-                    case CampaignItemStatus.Crawling:
-                    case CampaignItemStatus.CrawlingError:
-                    case CampaignItemStatus.NeedToEdit:
-                    case CampaignItemStatus.PendingApproval:
-                        Status = AdItemStatus.Pending;
-                        break;
-                    case CampaignItemStatus.Paused:
-                        Status = AdItemStatus.Paused;
-                        break;
-                    case CampaignItemStatus.Stopped:
-                        Status = AdItemStatus.Stopped;
-                        break;
-                    case CampaignItemStatus.Rejected:
-                        Status = AdItemStatus.Rejected;
-                        break;
-                }
-            }
-        }
-
-        public AdItemStatus Status { get; set; }
-        public string StatusText { get => Status.GetEnumMemberName(); }
-        public string Details { get; }
     }
 }
