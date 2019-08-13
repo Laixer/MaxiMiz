a2749d4846a55346216482aa9f343c42d1672010
tabeckers <tabeckers95@gmail.com>
2019-08-13 12:17:02 +0200

    Created template for bid modifiers as presented in API

---
+++ Poller/Poller.Taboola/Model/PublisherBidModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Poller.Taboola.Model
+{
+
+    /// <summary>
+    /// An object representing the targeted
+    /// publisher's bid modifiers.
+    /// </summary>
+    [DataContract]
+    internal class PublisherBidModifier
+    {
+
+        /// <summary>
+        /// Represents all targeted publisher's bid
+        /// modifiers.
+        /// 
+        /// Null represents no change to targeted
+        /// bid modifiers.
+        /// 
+        /// Empty array represents no targeted bid
+        /// modifiers.
+        /// </summary>
+        [DataMember(Name = "values")]
+        public TargetedBidModifier[] Values {get; set;}
+
+    }
+}
