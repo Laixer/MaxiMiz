b43d273226ce0c8aade07d24a88e340f01b83cb8
tabeckers <tabeckers95@gmail.com>
2019-08-15 17:17:39 +0200

    Added bulk conversion

--- Poller/Poller.Taboola/Mapper/IMapper.cs
+++ Poller/Poller.Taboola/Mapper/IMapper.cs
@@ -26,5 +26,21 @@ namespace Poller.Taboola.Mapper
         /// <returns>Core object</returns>
         TCore Convert(TExternal external);
 
+        /// <summary>
+        /// Convert a range from external to core.
+        /// </summary>
+        /// <param name="list">List of externals</param>
+        /// <returns>List of cores</returns>
+        IEnumerable<TCore> ConvertAll(
+            IEnumerable<TExternal> list);
+
+        /// <summary>
+        /// Convert a range from core to external.
+        /// </summary>
+        /// <param name="list">List of cores</param>
+        /// <returns>List of externals</returns>
+        IEnumerable<TExternal> ConvertAll(
+            IEnumerable<TCore> list);
+
     }
 }
