a2bf25a45d25bf3c8ed811528ace280cf8ac2e11
tabeckers <tabeckers95@gmail.com>
2019-08-20 14:37:17 +0200

    Added explaining doc

--- Poller/Poller.Taboola/Model/EntityList.cs
+++ Poller/Poller.Taboola/Model/EntityList.cs
@@ -3,6 +3,13 @@ using System.Runtime.Serialization;
 
 namespace Poller.Taboola.Model
 {
+
+    /// <summary>
+    /// This is the format in which the Taboola API 
+    /// returns their lists. This servers as our
+    /// deserialization template.
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
     [DataContract]
     internal class EntityList<TEntity>
         where TEntity : class
