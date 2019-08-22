c6bdaf829686dac78596a974e5578b94ba049303
6bq <31695264+6bq@users.noreply.github.com>
2019-08-22 14:05:57 +0200

    Create Device Enum

---
+++ Model/Enums/Device.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Maximiz.Model.Enums
+{
+
+    /// <summary>
+    /// Model enum to represent a device.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Device
+    {
+        [EnumMember(Value = "mobile")]
+        Mobile,
+        [EnumMember(Value = "tablet")]
+        Tablet,
+        [EnumMember(Value = "laptop")]
+        Laptop,
+        [EnumMember(Value = "desktop")]
+        Desktop,
+        [EnumMember(Value = "wearable")]
+        Wearable
+    }
+}
