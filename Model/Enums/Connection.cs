08d997b8659ddd2142beb41c71ca9f4461e9a899
6bq <31695264+6bq@users.noreply.github.com>
2019-08-23 15:34:05 +0200

    Connection Enum

---
+++ Model/Enums/Connection.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Maximiz.Model.Enums
+{
+
+    /// <summary>
+    /// Model enum to represent a connection.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Connection
+    {
+        [EnumMember(Value = "cable")]
+        Cable,
+        [EnumMember(Value = "wifi")]
+        WiFi,
+        [EnumMember(Value = "cellular")]
+        Cellular
+    }
+}
