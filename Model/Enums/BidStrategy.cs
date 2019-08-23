832dd728225d77407b362e854d819b4e6a3af489
6bq <31695264+6bq@users.noreply.github.com>
2019-08-23 17:19:19 +0200

    Bid Strategy Enum

---
+++ Model/Enums/BidStrategy.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Maximiz.Model.Enums
+{
+
+    /// <summary>
+    /// Model enum to represent a Bid Strategy.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum BidStrategy
+    {
+        [EnumMember(Value = "smart")]
+        Smart,
+        [EnumMember(Value = "fixed")]
+        Fixed
+    }
+}
