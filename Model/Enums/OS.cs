74b2ca34da5ace61129b26c0f571b3bcadd02f74
6bq <31695264+6bq@users.noreply.github.com>
2019-08-23 15:33:49 +0200

    Operating System Enum

---
+++ Model/Enums/OS.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Maximiz.Model.Enums
+{
+
+    /// <summary>
+    /// Model enum to represent an operating system.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum OS
+    {
+        [EnumMember(Value = "windows")]
+        Windows,
+        [EnumMember(Value = "linux")]
+        Linux,
+        [EnumMember(Value = "osx")]
+        OSX,
+        [EnumMember(Value = "android")]
+        Android,
+        [EnumMember(Value = "ios")]
+        IOS,
+        [EnumMember(Value = "unix")]
+        Unix,
+        [EnumMember(Value = "chromeos")]
+        ChromeOS
+    }
+}
