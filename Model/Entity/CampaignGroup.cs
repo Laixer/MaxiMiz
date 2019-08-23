6bc7fe3319c258ca6e28c2121973a88aa983aebe
6bq <31695264+6bq@users.noreply.github.com>
2019-08-23 17:21:09 +0200

    Create CampaignGroup.cs

---
+++ Model/Entity/CampaignGroup.cs
@@ -0,0 +1,100 @@
+﻿using Maximiz.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximiz.Model.Entity
+{
+    /// <summary>
+    /// Campaign Group.
+    /// </summary>
+    public class CampaignGroup : EntityAudit<int>
+    {
+        /// <summary>
+        /// Campaign name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Campaign branding text.
+        /// </summary>
+        public string BrandingText { get; set; }
+
+        /// <summary>
+        /// Region in which campaign is active.
+        /// </summary>
+        public int[] LocationInclude { get; set; }
+
+        /// <summary>
+        /// Region in which campaign is not active.
+        /// </summary>
+        public int[] LocationExclude { get; set; }
+
+        /// <summary>
+        /// Language of the campaign, 2 chars.
+        /// TODO Why do we need this?
+        /// </summary>
+        public string[] Language { get; set; }
+
+        /// <summary>
+        /// Targeted devices.
+        /// </summary>
+        public Device[] Devices { get; set; }
+
+        /// <summary>
+        /// Targeted operating systems.
+        /// </summary>
+        public OS[] OperatingSystems { get; set; }
+
+        /// <summary>
+        /// The initial CPC per item.
+        /// </summary>
+        public decimal InitialCpc { get; set; }
+
+        /// <summary>
+        /// Budget per campaign or period.
+        /// </summary>
+        public decimal Budget { get; set; }
+
+        /// <summary>
+        /// Budget per day. Can be null.
+        /// </summary>
+        public decimal? DailyBudget { get; set; }
+
+        /// <summary>
+        /// Delivery mode of this ad.
+        /// </summary>
+        public Delivery Delivery { get; set; }
+        public string DeliveryText { get => Delivery.GetEnumMemberName(); }
+
+        /// <summary>
+        /// Campaign start date.
+        /// </summary>
+        public DateTime? StartDate { get; set; }
+
+        /// <summary>
+        /// Campaign end date.
+        /// </summary>
+        public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// Status.
+        /// </summary>
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// Tracking code.
+        /// </summary>
+        public string Utm { get; set; }
+
+        /// <summary>
+        /// Note.
+        /// </summary>
+        public string Note { get; set; }
+
+        /// <summary>
+        /// Connections.
+        /// </summary>
+        public Connection[] Connections { get; set; }
+    }
+}
