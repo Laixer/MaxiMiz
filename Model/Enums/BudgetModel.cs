89c0c8a7cf25cf904f2e8930d676abe75cce377e
6bq <31695264+6bq@users.noreply.github.com>
2019-08-26 13:43:56 +0200

    Add BudgetModel enum

---
+++ Model/Enums/BudgetModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Maximiz.Model.Enums
+{
+
+    /// <summary>
+    /// Model enum to represent the budget model of a campaign.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum BudgetModel
+    {
+        [EnumMember(Value = "campaign")]
+        Campaign,
+        [EnumMember(Value = "monthly")]
+        Monthly
+    }
+}
