180a1375b0bee1e0d19c11a8b0577829f3afa3e5
tabeckers <tabeckers95@gmail.com>
2019-08-28 10:17:38 +0200

    TargetType enum to its own file

---
+++ Poller/Poller.Taboola/Model/TargetType.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Poller.Taboola.Model
+{
+
+    /// <summary>
+    /// Enum to indicate the way we should interpret
+    /// the value string array.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TargetType
+    {
+
+        /// <summary>
+        /// All are included.
+        /// </summary>
+        [EnumMember(Value = "include")]
+        Include,
+
+        /// <summary>
+        /// All are excluded.
+        /// </summary>
+        [EnumMember(Value = "exclude")]
+        Exclude,
+
+        /// <summary>
+        /// All.
+        /// </summary>
+        [EnumMember(Value = "all")]
+        All
+    }
+
+}
