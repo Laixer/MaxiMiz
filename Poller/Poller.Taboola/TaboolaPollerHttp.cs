54269633315db551edbc99cc8e974a400eac1ffa
tabeckers <tabeckers95@gmail.com>
2019-09-09 13:41:04 +0200

    Removed unused imports

--- Poller/Poller.Taboola/TaboolaPollerHttp.cs
+++ Poller/Poller.Taboola/TaboolaPollerHttp.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.Extensions.Logging;
-using Poller.Taboola.Model;
-
-using AccountCore = Maximiz.Model.Entity.Account;
-using CampaignCore = Maximiz.Model.Entity.Campaign;
-using AdItemCore = Maximiz.Model.Entity.AdItem;
-using Poller.Helper;
-using System.Linq;
-using System.Diagnostics;
 
 namespace Poller.Taboola
 {
