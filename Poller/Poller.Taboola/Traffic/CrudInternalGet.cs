1e2ce3b44c6ab3e38fec360afd8d8ab2dd3ae55e
tabeckers <tabeckers95@gmail.com>
2019-09-23 11:02:00 +0200

    Moved database fetcher to separate class. Created account type enum
    Getting account types is now done based on the account type enum. This is a bit cleaner and more future proof.

--- Poller/Poller.Taboola/Traffic/CrudInternalGet.cs
+++ Poller/Poller.Taboola/Traffic/CrudInternalGet.cs
@@ -7,6 +7,10 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using CampaignInternal = Maximiz.Model.Entity.Campaign;
+using AccountInternal = Maximiz.Model.Entity.Account;
+using AdItemInternal = Maximiz.Model.Entity.AdItem;
+using Microsoft.Extensions.Caching.Memory;
+using Poller.Taboola.Model;
 
 namespace Poller.Taboola.Traffic
 {
@@ -16,6 +20,91 @@ namespace Poller.Taboola.Traffic
     /// </summary>
     internal partial class CrudInternal
     {
+
+        /// <summary>
+        /// Gets a campaign from our local database based on the GUID.
+        /// </summary>
+        /// <remarks>Returns null if item is not found</remarks>
+        /// <param name="guid">The campaign GUID</param>
+        /// <param name="token">The cancellation token</param>
+        /// <returns>Our internal database campaign</returns>
+        public async Task<CampaignInternal> GetCampaignFromGuid(
+            Guid guid, CancellationToken token)
+        {
+            var sql = $"SELECT * FROM public.campaign WHERE id::text = '{guid.ToString()}';";
+            using (var connection = _dbProvider.ConnectionScope())
+            {
+                var result = await connection.QueryAsync<CampaignInternal>(
+                    new CommandDefinition(sql, cancellationToken: token));
+                if (result.ToArray().Length > 0)
+                {
+                    var x = result.ToArray()[0] as CampaignInternal;
+                    return x;
+                }
+                else { return null; }
+            }
+        }
+
+        /// <summary>
+        /// Gets all advertiser accounts from our cache. If this data is not
+        /// present, it is polled from our local database and stored in the
+        /// cache.
+        /// </summary>
+        /// <param name="token">The cancellation token</param>
+        /// <returns>All advertiser accounts</returns>
+        private Task<IEnumerable<AccountInternal>>
+            GetAdvertiserAccountsCachedAsync(CancellationToken token)
+        {
+            return GetAccountsCachedAsync(AccountType.Advertiser, token);
+        }
+
+        /// <summary>
+        /// Gets all publisher accounts from our cache. If this data is not
+        /// present, it is polled from our local database and stored in the
+        /// cache.
+        /// </summary>
+        /// <param name="token">The cancellation token</param>
+        /// <returns>All publisher accounts</returns>
+        private Task<IEnumerable<AccountInternal>>
+            GetPublisherAccountsCachedAsync(CancellationToken token)
+        {
+            return GetAccountsCachedAsync(AccountType.Publisher, token);
+        }
+
+        /// <summary>
+        /// Gets all accounts of specified type from our cache. If this data
+        /// is not present, it is polled from our local database and stored in 
+        /// the cache.
+        /// TODO This is not bulletproof for long enum names
+        /// </summary>
+        /// <param name="accountType">The account type</param>
+        /// <param name="token">The cancellation token</param>
+        /// <returns>All publisher accounts</returns>
+        private async Task<IEnumerable<AccountInternal>> GetAccountsCachedAsync(
+            AccountType accountType, CancellationToken token)
+        {
+            return await _cache.GetOrCreateAsync(
+                "AdvertiserAccounts", async entry =>
+                {
+                    entry.SlidingExpiration = TimeSpan.FromDays(1);
+
+                    var sql = @"
+                        SELECT
+                            *
+	                    FROM
+                            public.account
+                        WHERE
+                            publisher = 'taboola'::publisher AND
+                            (details::json #>> '{partner_types}')::jsonb ? '"
+                            + accountType.ToString().ToLower() + "'";
+
+                    using (var connection = _dbProvider.ConnectionScope())
+                    {
+                        return await connection.QueryAsync<AccountInternal>
+                            (new CommandDefinition(sql, cancellationToken: token));
+                    }
+                });
+        }
 
 
     }
