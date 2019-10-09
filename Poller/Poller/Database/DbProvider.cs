d4010b80d4e13536d4098445df619a6a749b3e51
tabeckers <tabeckers95@gmail.com>
2019-10-09 13:42:38 +0200

    Implemented explicit enum mapping for database connections

--- Poller/Poller/Database/DbProvider.cs
+++ Poller/Poller/Database/DbProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using Maximiz.Model.Enums;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Npgsql;
@@ -12,9 +13,24 @@ namespace Poller.Database
 
         public IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// Static instance of the database provider.
+        /// </summary>
         static DbProvider()
         {
             Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<AdItemStatus>("ad_item_status");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<ApprovalState>("approval_state");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<BidStrategy>("bid_strategy");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<BudgetModel>("budget_model");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<CampaignStatus>("campaign_status");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<ConnectionType>("connection");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<Location>("location");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<Delivery>("delivery");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<Device>("device");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<OS>("operating_system");
+            NpgsqlConnection.GlobalTypeMapper.MapEnum<Maximiz.Model.Enums.Publisher>("publisher");
         }
 
         /// <summary>
@@ -29,10 +45,24 @@ namespace Poller.Database
             connectionString = Configuration.GetConnectionString(_options.ConnectionStringName);
         }
 
+
+        /// <summary>
+        /// Constructor for testing purposes.
+        /// TODO Might want to remove this.
+        /// </summary>
+        /// <param name="connectionString">The connection string</param>
+        public DbProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
         /// <summary>
         /// Create a new connection instance.
         /// </summary>
         /// <returns><see cref="IDbConnection"/> instance.</returns>
-        public IDbConnection ConnectionScope() => new NpgsqlConnection(connectionString);
+        public IDbConnection ConnectionScope()
+        {
+            return new NpgsqlConnection(connectionString);
+        }
     }
 }
