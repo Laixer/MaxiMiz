40583dd7b10b8300766ddddf7db173d2d9490fb2
tabeckers <tabeckers95@gmail.com>
2019-10-15 13:51:45 +0200

    Created local database creation abstraction

---
+++ Web/Transactions/Creation/AdGroupCreator.cs
@@ -0,0 +1,21 @@
+﻿using Maximiz.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maximiz.Transactions.Creation
+{
+    public class AdGroupCreator : ICreator<AdGroup>
+    {
+        public Task<AdGroup> Create(AdGroup entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AdGroup>> CreateAll(IEnumerable<AdGroup> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
