2dad443d7c649b3e2efaadff2602f6d41e53f85b
tabeckers <tabeckers95@gmail.com>
2019-10-15 13:52:17 +0200

    Created transaction abstraction and (first part of) implementation

---
+++ Web/Transactions/EntityTransaction.cs
@@ -0,0 +1,43 @@
+﻿using Maximiz.Model.Entity;
+using System.Collections.Generic;
+
+namespace Maximiz.Transactions
+{
+
+    /// <summary>
+    /// Holds information over one single entity transaction.
+    /// TODO How to manage account?
+    /// </summary>
+    public class EntityTransaction
+    {
+
+        /// <summary>
+        /// List of all entities to CRUD.
+        /// </summary>
+        public IEnumerable<Entity> AllEntities { get; set; }
+
+        /// <summary>
+        /// True if completed successfully.
+        /// </summary>
+        private bool completed { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="allEntities">All entities to CRUD</param>
+        public EntityTransaction(IEnumerable<Entity> allEntities)
+        {
+            AllEntities = allEntities;
+            completed = false;
+        }
+
+        /// <summary>
+        /// Marks the transaction as complete.
+        /// </summary>
+        public void MarkAsComplete() => completed = true;
+
+        /// <returns>True if the transaction completed successfully</returns>
+        public bool IsCompleted() => completed;
+
+    }
+}
---
+++ Web/Transactions/ITransactionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Maximiz.Transactions
+{
+
+    /// <summary>
+    /// Contract for handling an entity transaction.
+    /// </summary>
+    public interface ITransactionHandler
+    {
+
+        /// <summary>
+        /// Handles an entity transaction async. This first waits for database
+        /// creation, then for service bus message sent, then returns completion.
+        /// </summary>
+        /// <param name="transaction">The transaction to process</param>
+        /// <returns>True if successful</returns>
+        Task<bool> HandleTransaction(EntityTransaction transaction);
+
+    }
+}
