82b7a7ec0171b0da06fb57baf69f8111d39d9d38
tabeckers <tabeckers95@gmail.com>
2019-10-15 13:53:04 +0200

    Updated dependency injection

---
+++ Web/Extensions/TransactionHandlerServiceCollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Maximiz.Model.Entity;
+using Maximiz.Transactions;
+using Maximiz.Transactions.Creation;
+using Maximiz.Transactions.ServiceBus;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+
+    /// <summary>
+    /// Transaction handler dependency injection extension.
+    /// </summary>
+    public static class TransactionHandlerServiceCollectionExtensions
+    {
+
+        /// <summary>
+        /// Adds a transaction handler to the service collection.
+        /// </summary>
+        /// <typeparam name="TTransactionHandler">The transaction handler type</typeparam>
+        /// <param name="services">The service collection</param>
+        /// <returns>The service collection</returns>
+        public static IServiceCollection AddTransactionHandler
+            <TTransactionHandler>(this IServiceCollection services)
+            where TTransactionHandler : class, ITransactionHandler
+        {
+            // First add creation dependencies
+            services.AddSingleton<ICreator<Entity>, EntityCreator>();
+            services.AddSingleton<ISender<Entity>, Sender>();
+
+            // Then add the transaction handler
+            services.AddSingleton<ITransactionHandler, TTransactionHandler>();
+
+            // Return services for chaining
+            return services;
+        }
+
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -3,13 +3,27 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Maximiz
 {
+
+    /// <summary>
+    /// Application main entry class to setup host building.
+    /// </summary>
     public class Program
     {
+
+        /// <summary>
+        /// Application main entry point.
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
         public static void Main(string[] args)
             => CreateWebHostBuilder(args).Build().Run();
 
+        /// <summary>
+        /// Instantiates a host builder.
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
+        /// <returns>A web host builder</returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+
     }
 }
