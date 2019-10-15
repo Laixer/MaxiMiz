5cb85854a90ed40927bc65501f3dfa4059849d3c
tabeckers <tabeckers95@gmail.com>
2019-10-15 13:51:59 +0200

    Created service bus abstraction and implementation

---
+++ Web/Transactions/ServiceBus/ISender.cs
@@ -0,0 +1,26 @@
+﻿using Maximiz.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maximiz.Transactions.ServiceBus
+{
+
+    /// <summary>
+    /// Contract for sending entities to a service bus through messages.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public interface ISender<TEntity>
+        where TEntity : Entity
+    {
+
+        /// <summary>
+        /// Sends an entity through the service bus.
+        /// </summary>
+        /// <param name="entity">The entity to send</param>
+        /// <returns>Task</returns>
+        Task Send(TEntity entity);
+
+    }
+}
