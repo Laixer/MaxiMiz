be41b27825708b1fe756878f8b53e9a0329b6538
tabeckers <tabeckers95@gmail.com>
2019-10-17 17:06:51 +0200

    Created separate models for views in MVC
    Even though these models are mostly similar to our Maximiz.Entity models, they still are separate entities and thus separate classes.

---
+++ Web/Models/AdGroup.cs
@@ -0,0 +1,34 @@
+﻿using Maximiz.Model.Mvc.Entity;
+using System;
+
+namespace Maximiz.Mvc.Models
+{
+
+    /// <summary>
+    /// Represents an ad group along with some numeric data.
+    /// </summary>
+    public class AdGroup : EntityAudit<Guid>
+    {
+
+        /// <summary>
+        /// Group name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// User description to describe this ad group.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Array of image links.
+        /// </summary>
+        public string[] ImageLinks { get; set; }
+
+        /// <summary>
+        /// Array of titles.
+        /// </summary>
+        public string[] Titles { get; set; }
+
+    }
+}
