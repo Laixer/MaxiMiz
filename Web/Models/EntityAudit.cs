b6cb87a82484d1f595f482f32b5fc03dbf45f4e1
tabeckers <tabeckers95@gmail.com>
2019-10-17 17:06:51 +0200

    Created separate models for views in MVC
    Even though these models are mostly similar to our Maximiz.Entity models, they still are separate entities and thus separate classes.

---
+++ Web/Models/EntityAudit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Maximiz.Model.Mvc.Entity
+{
+    /// <summary>
+    /// Create, update, delete tracking entity.
+    /// </summary>
+    /// <typeparam name="TPrimary">Primary key.</typeparam>
+    [Serializable]
+    public abstract class EntityAudit<TPrimary> : Entity<TPrimary>
+    {
+        /// <summary>
+        /// Timestamp of creation.
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Timestamp of last update.
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+
+        /// <summary>
+        /// Timestamp of deletion.
+        /// </summary>
+        public DateTime? DeleteDate { get; set; }
+    }
+}
