22fc329ab80e9970212d0e074d7d7fdcedb22b5b
tabeckers <tabeckers95@gmail.com>
2019-10-22 16:19:28 +0200

    Created models for views

---
+++ Model/Entity/AdGroupWithStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Maximiz.Model.Enums;
+
+namespace Maximiz.Model.Entity
+{
+    /// <summary>
+    /// Represents a <see cref="AdGroup"/> along with some calculated statistics.
+    /// This entity is obtained from one of our database views.
+    /// </summary>
+    [Serializable]
+    public class AdGroupWithStats : AdGroup
+    { 
+
+        // TODO Add properties from view
+
+    }
+}
---
+++ Model/Entity/CampaignWithStats.cs
@@ -0,0 +1,58 @@
+﻿using Maximiz.Model.Enums;
+using System;
+
+namespace Maximiz.Model.Entity
+{
+    /// <summary>
+    /// Represents a <see cref="Campaign"/> along with some calculated statistics.
+    /// This entity is obtained from one of our database views.
+    /// </summary>
+    [Serializable]
+    public class CampaignWithStats : Campaign
+    {
+
+        // TODO Make sure list of stats is complete.
+
+        /// <summary>
+        /// Click through rate, average of all campaign items in this campaign.
+        /// </summary>
+        public decimal? Ctr { get; set; }
+
+        /// <summary>
+        /// Return on investment.
+        /// </summary>
+        public double? Roi { get; set; }
+
+        /// <summary>
+        /// Total revenue of this campaign.
+        /// </summary>
+        public decimal? Revenue { get; set; }
+
+        /// <summary>
+        /// Total revenue from Taboola publishers.
+        /// </summary>
+        public decimal? RevenueTaboola { get; set; }
+
+        /// <summary>
+        /// Total revenue from Adsense publishers.
+        /// </summary>
+        public decimal? RevenueAdsense { get; set; }
+
+        /// <summary>
+        /// Total profit of this campaign.
+        /// </summary>
+        public decimal? Profit { get; set; }
+
+        /// <summary>
+        /// Total actions of all ad items in this campaign.
+        /// </summary>
+        public int Actions { get; set; }
+
+        /// <summary>
+        /// Total amount of clicks as sum of all ad items in this campaign.
+        /// </summary>
+        public int Clicks { get; set; }
+
+    }
+
+}
