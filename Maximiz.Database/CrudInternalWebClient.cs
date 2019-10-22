70ceb0c4aea04517f1429c033cc9c66919b8083c
tabeckers <tabeckers95@gmail.com>
2019-10-22 16:18:37 +0200

    Updated database abstraction and (part of) implementation

---
+++ Maximiz.Database/CrudInternalWebClient.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Maximiz.Database.Querying;
+using Maximiz.Model.Entity;
+
+namespace Maximiz.Database
+{
+
+    /// <summary>
+    /// TODO Implement.
+    /// </summary>
+    public class CrudInternalWebClient : CrudInternal, ICrudInternalWebClient
+    {
+        public Task<AdGroupWithStats> CreateAdGroupAsync(AdGroup adGroup, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdItemWithStats> CreateAdItemAsync(AdItem adItem, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampaignWithStats> CreateCampaignAsync(Campaign campaign, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampaignGroupWithStats> CreateCampaignGroupAsync(CampaignGroup campaignGroup, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdGroupWithStats> DeleteAdGroupAsync(AdGroup adGroup, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampaignGroupWithStats> DeleteCampaignGroupAsync(CampaignGroup campaignGroup, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdGroupWithStats> GetAdGroupAsync(Guid guid, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AdGroupWithStats>> GetAdGroupsAsync(Query<ColumnAdGroupWithStats> query, int page, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AdItemWithStats>> GetAdItemsAsync(Query<ColumnAdItemWithStats> query, int page, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdItemWithStats> GetAdItemWithStatsAsync(Guid guid, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampaignGroupWithStats> GetCampaignGroupAsync(Guid guid, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CampaignGroupWithStats>> GetCampaignGroupsAsync(Query<ColumnCampaignGroupWithStats> query, int page, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CampaignWithStats>> GetCampaignsAsync(Query<ColumnCampaignWithStats> query, int page, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampaignWithStats> GetCampaignWithStatsAsync(Guid guid, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdGroupWithStats> UpdateAdGroupAsync(AdGroup adGroup, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CampaignGroupWithStats> UpdateCampaignGroupAsync(CampaignGroup campaignGroup, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Maximiz.Database/ICrudInternalWebClient.cs
@@ -0,0 +1,50 @@
+﻿using Maximiz.Model.Entity;
+using Maximiz.Database.Querying;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Maximiz.Database
+{
+
+    /// <summary>
+    /// Contract for database operations for our webclient.
+    /// </summary>
+    public interface ICrudInternalWebClient : ICrudInternal
+    {
+
+        Task<CampaignWithStats> GetCampaignWithStatsAsync(Guid guid, CancellationToken token);
+
+        Task<AdItemWithStats> GetAdItemWithStatsAsync(Guid guid, CancellationToken token);
+
+        Task<CampaignGroupWithStats> GetCampaignGroupAsync(Guid guid, CancellationToken token);
+
+        Task<AdGroupWithStats> GetAdGroupAsync(Guid guid, CancellationToken token);
+
+        Task<IEnumerable<CampaignWithStats>> GetCampaignsAsync(Query<ColumnCampaignWithStats> query, int page, CancellationToken token);
+
+        Task<IEnumerable<CampaignGroupWithStats>> GetCampaignGroupsAsync(Query<ColumnCampaignGroupWithStats> query, int page, CancellationToken token);
+
+        Task<IEnumerable<AdItemWithStats>> GetAdItemsAsync(Query<ColumnAdItemWithStats> query, int page, CancellationToken token);
+
+        Task<IEnumerable<AdGroupWithStats>> GetAdGroupsAsync(Query<ColumnAdGroupWithStats> query, int page, CancellationToken token);
+
+        Task<CampaignWithStats> CreateCampaignAsync(Campaign campaign, CancellationToken token);
+
+        Task<CampaignGroupWithStats> CreateCampaignGroupAsync(CampaignGroup campaignGroup, CancellationToken token);
+
+        Task<AdItemWithStats> CreateAdItemAsync(AdItem adItem, CancellationToken token);
+
+        Task<AdGroupWithStats> CreateAdGroupAsync(AdGroup adGroup, CancellationToken token);
+
+        Task<CampaignGroupWithStats> UpdateCampaignGroupAsync(CampaignGroup campaignGroup, CancellationToken token);
+
+        Task<AdGroupWithStats> UpdateAdGroupAsync(AdGroup adGroup, CancellationToken token);
+
+        Task<CampaignGroupWithStats> DeleteCampaignGroupAsync(CampaignGroup campaignGroup, CancellationToken token);
+
+        Task<AdGroupWithStats> DeleteAdGroupAsync(AdGroup adGroup, CancellationToken token);
+
+    }
+}
