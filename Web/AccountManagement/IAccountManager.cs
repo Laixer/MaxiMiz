d36fd6166460df2818cce1d51399cf2e90718728
tabeckers <tabeckers95@gmail.com>
2019-10-22 16:22:20 +0200

    First implementation iteration for account management

---
+++ Web/AccountManagement/IAccountManager.cs
@@ -0,0 +1,23 @@
+﻿using Maximiz.Model.Entity;
+using Maximiz.Model.Enums;
+
+namespace Maximiz.AccountManagement
+{
+
+    /// <summary>
+    /// Interface for keeping track of which accounts we currently want our 
+    /// created entities to belong to. The implementation should simply store 
+    /// the selected accounts for each publisher.
+    /// </summary>
+    internal interface IAccountManager
+    {
+
+        /// <summary>
+        /// Gets the account for a given <see cref="Publisher"/>.
+        /// </summary>
+        /// <param name="publisher"><see cref="Publisher"/></param>
+        /// <returns>The corresponding <see cref="Account"/></returns>
+        Account GetSelectedPublisherAccount(Publisher publisher);
+
+    }
+}
