89cecd2b212588e4e15eb2b45d122cf634c0e05d
tabeckers <tabeckers95@gmail.com>
2019-11-08 16:06:54 +0100

    Created enum translation for UI display

---
+++ Web/Translation/EnumTranslator.cs
@@ -0,0 +1,100 @@
+﻿using Maximiz.ViewModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maximiz.Translation
+{
+
+    /// <summary>
+    /// Contains functionality to convert a viewmodel enum to the corresponding
+    /// string value for UI display purposes.
+    /// </summary>
+    public static class EnumTranslator
+    {
+
+        /// <summary>
+        /// Translates a <see cref="BidStrategy"/> to a string.
+        /// </summary>
+        /// <param name="bidStrategy"><see cref="BidStrategy"/></param>
+        /// <returns>Corresponding string value</returns>
+        public static string TranslateBidStrategy(BidStrategy bidStrategy)
+        {
+            switch (bidStrategy)
+            {
+                case BidStrategy.Smart:
+                    return "Smart";
+                case BidStrategy.Fixed:
+                    return "Fixed";
+            }
+
+            throw new InvalidOperationException(nameof(bidStrategy));
+        }
+
+        /// <summary>
+        /// Translates a <see cref="Delivery"/> to a string.
+        /// </summary>
+        /// <param name="delivery"><see cref="Delivery"/></param>
+        /// <returns>Corresponding string value</returns>
+        public static string TranslateDelivery(Delivery delivery)
+        {
+            switch (delivery)
+            {
+                case Delivery.Balanced:
+                    return "Balanced";
+                case Delivery.Accelerated:
+                    return "Accelerated";
+                case Delivery.Strict:
+                    return "Strict";
+            }
+
+            throw new InvalidOperationException(nameof(delivery));
+        }
+
+        /// <summary>
+        /// Translates a <see cref="Publisher"/> to a string.
+        /// </summary>
+        /// <param name="publisher"><see cref="Publisher"/></param>
+        /// <returns>Corresponding string value</returns>
+        public static string TranslatePublisher(Publisher publisher)
+        {
+            switch (publisher)
+            {
+                case Publisher.Unknown:
+                    return "Unknown";
+                case Publisher.Google:
+                    return "Google";
+                case Publisher.Taboola:
+                    return "Taboola";
+                case Publisher.Outbrain:
+                    return "Outbrain";
+                case Publisher.Adroll:
+                    return "Adroll";
+                case Publisher.Criteo:
+                    return "Criteo";
+            }
+
+            throw new InvalidOperationException(nameof(publisher));
+        }
+
+        /// <summary>
+        /// Translates a <see cref="BudgetModel"/> to a string.
+        /// </summary>
+        /// <param name="budgetModel"><see cref="BudgetModel"/></param>
+        /// <returns>Corresponding string value</returns>
+        public static string TranslateBudgetModel(BudgetModel budgetModel)
+        {
+            switch (budgetModel)
+            {
+                case BudgetModel.Campaign:
+                    return "Campaign";
+                case BudgetModel.Monthly:
+                    return "Monthly";
+            }
+
+            throw new InvalidOperationException(nameof(budgetModel));
+        }
+
+    }
+}
---
+++ Web/Translation/LocationTranslator.cs
@@ -0,0 +1,91 @@
+﻿using Maximiz.ViewModels.Enums;
+using System;
+
+namespace Maximiz.Translation
+{
+
+    /// <summary>
+    /// Translates our <see cref="Location"/>s to a human readable format.
+    /// </summary>
+    public static class LocationTranslator
+    {
+
+        /// <summary>
+        /// Converts a <see cref="Location"/> to a human readable string.
+        /// </summary>
+        /// <param name="location"><see cref="Location"/></param>
+        /// <returns>Human readable string</returns>
+        public static string TranslateLocation(Location location)
+        {
+            switch (location)
+            {
+                case Location.NL:
+                    return "Netherlands";
+                case Location.UK:
+                    return "United Kingdom";
+                case Location.ES:
+                    return "Spain";
+                case Location.DE:
+                    return "Germany";
+                case Location.FR:
+                    return "Le country du baguette";
+                    // TODO fix this lol
+            }
+
+            throw new InvalidOperationException(nameof(location));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="OS"/> to a human readable string.
+        /// </summary>
+        /// <param name="operatingSystem"><see cref="OS"/></param>
+        /// <returns>Human readable string</returns>
+        public static string TranslateOperatingSystem(OS operatingSystem)
+        {
+            switch (operatingSystem)
+            {
+                case OS.Windows:
+                    return "Windows";
+                case OS.Linux:
+                    return "Linux";
+                case OS.OSX:
+                    return "Apple OSX";
+                case OS.Android:
+                    return "Android";
+                case OS.iOS:
+                    return "Apple iOS";
+                case OS.Unix:
+                    return "Unix";
+                case OS.Chromeos:
+                    return "Chrome OS";
+            }
+
+            throw new InvalidOperationException(nameof(operatingSystem));
+        }
+
+        /// <summary>
+        /// Converts a <see cref="Device"/> to a human readable string.
+        /// </summary>
+        /// <param name="device"><see cref="Device"/></param>
+        /// <returns>Human readable string</returns>
+        public static string TranslateDevice(Device device)
+        {
+            switch (device)
+            {
+                case Device.Mobile:
+                    return "Mobile";
+                case Device.Tablet:
+                    return "Tablet";
+                case Device.Laptop:
+                    return "Laptop";
+                case Device.Desktop:
+                    return "Desktop";
+                case Device.Wearable:
+                    return "Wearable";
+            }
+
+            throw new InvalidOperationException(nameof(device));
+        }
+
+    }
+}
