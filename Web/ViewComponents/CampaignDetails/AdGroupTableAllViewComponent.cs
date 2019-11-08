5d106947434bbd5859b4ad83f9f07c8b5885e7e7
tabeckers <tabeckers95@gmail.com>
2019-11-08 16:07:18 +0100

    Created view components for dynamic elements

---
+++ Web/ViewComponents/CampaignDetails/AdGroupTableAllViewComponent.cs
@@ -0,0 +1,68 @@
+﻿using Maximiz.Database.Querying;
+using Maximiz.Mapper;
+using Maximiz.Model.Entity;
+using Maximiz.Repositories.Abstraction;
+using Maximiz.ViewModels.CampaignDetails;
+using Maximiz.ViewModels.EntityModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maximiz.ViewComponents.CampaignOverview
+{
+
+    /// <summary>
+    /// View component for loading our campaign tables async.
+    /// </summary>
+    public class AdGroupTableAllViewComponent : ViewComponent
+    {
+
+        /// <summary>
+        /// Data store for ad groups.
+        /// </summary>
+        private readonly IAdGroupRepository _adGroupRepository;
+
+        /// <summary>
+        /// Maps our ad groups.
+        /// </summary>
+        private readonly IMapper<AdGroupWithStats, AdGroupModel> _mapperAdGroups;
+
+        /// <summary>
+        /// Constructor for dependency injection.
+        /// </summary>
+        public AdGroupTableAllViewComponent(IAdGroupRepository adGroupRepository,
+             IMapper<AdGroupWithStats, AdGroupModel> mapperAdGroups)
+        {
+            _adGroupRepository = adGroupRepository;
+            _mapperAdGroups = mapperAdGroups;
+        }
+
+        /// <summary>
+        /// Invokes this view component and fetches a given query from the data
+        /// store.
+        /// TODO Who is going to translate the query object?
+        /// </summary>
+        /// <param name="campaignId">The internal id of the linked campaign</param>
+        /// <param name="query"><see cref="QueryAdGroupWithStats"/></param>
+        /// <returns>Partial view containing a table</returns>
+        public async Task<IViewComponentResult> InvokeAsync(Guid campaignId, QueryAdGroupWithStats query)
+        {
+            // Simulate waiting
+            await Task.Delay(new Random().Next(200, 800));
+
+            // TODO Remove assignment??
+            var all = _mapperAdGroups.ConvertAll(await _adGroupRepository.GetAllAsync(query));
+            var linkedIds = all.ToList().GetRange(1, 2).Select(x => x.Id); // TODO We need the campaignId here
+            var model = new AdGroupTableAllViewModel
+            {
+                AdGroupsAll = all,
+                AdGroupIdsLinked = linkedIds,
+                TotalCount = await _adGroupRepository.GetCount(query)
+            };
+
+            return View("TableRows", model);
+        }
+
+    }
+}
---
+++ Web/ViewComponents/CampaignOverview/CampaignCountViewComponent.cs
@@ -0,0 +1,80 @@
+﻿using Maximiz.Database.Querying;
+using Maximiz.Mapper.Enum;
+using Maximiz.Repositories.Abstraction;
+using Maximiz.ViewModels.CampaignOverview;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Maximiz.ViewComponents.CampaignOverview
+{
+
+    /// <summary>
+    /// View component for getting the total amount of campaigns for any given
+    /// query.
+    /// </summary>
+    public class CampaignCountViewComponent : ViewComponent
+    {
+
+        /// <summary>
+        /// Data store for campaigns.
+        /// </summary>
+        private readonly ICampaignRepository _campaignRepository;
+
+        /// <summary>
+        /// Constructor for dependency injection.
+        /// </summary>
+        public CampaignCountViewComponent(ICampaignRepository campaignRepository)
+        {
+            _campaignRepository = campaignRepository;
+        }
+
+        /// <summary>
+        /// Invokes this view component and fetches a given query from the data
+        /// store.
+        /// TODO Who is going to translate the query object?
+        /// </summary>
+        /// <param name="table">The table type</param>
+        /// <param name="query"><see cref="QueryCampaignWithStats"/></param>
+        /// <returns>Partial view containing a table</returns>
+        public async Task<IViewComponentResult> InvokeAsync(CampaignTableType table, QueryCampaignWithStats query)
+        {
+            // Simulate waiting
+            await Task.Delay(new Random().Next(25, 100));
+
+            return View("TableCount", new CampaignCountViewModel
+            {
+                TableName = MapperEnum.TranslateCampaignTableType(table),
+                CampaignCount = await GetCampaignCount(table, query)
+            });
+        }
+
+        /// <summary>
+        /// Returns the campaign count that belong to a table, query and page.
+        /// </summary>
+        /// <param name="table"><see cref="CampaignTableType"/></param>
+        /// <param name="query"><see cref="QueryCampaignWithStats"/></param>
+        /// <param name="page">Page number</param>
+        /// <returns>Task that retreives the campaign count</returns>
+        private Task<int> GetCampaignCount(CampaignTableType table, QueryCampaignWithStats query)
+        {
+            switch (table)
+            {
+                case CampaignTableType.All:
+                    return _campaignRepository.GetCount(query);
+                case CampaignTableType.Active:
+                    // TODO Add active to search string
+                    return _campaignRepository.GetCount(query);
+                case CampaignTableType.Inactive:
+                    // TODO Add inactive to search string
+                    return _campaignRepository.GetCount(query);
+                case CampaignTableType.Pending:
+                    // TODO Add pending to search string
+                    return _campaignRepository.GetCount(query);
+            }
+
+            throw new InvalidOperationException(nameof(table));
+        }
+
+    }
+}
