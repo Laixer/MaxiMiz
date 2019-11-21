8dae9b0151fbd881263bf0be8dacfcf3fccc0edb
tabeckers <tabeckers95@gmail.com>
2019-11-21 17:33:34 +0100

    Implemented storage

---
+++ Web/Storage/ConnectionStringExtractor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maximiz.Storage
+{
+
+    /// <summary>
+    /// Extracts parameters from a connection string.
+    /// TODO Place in another package.
+    /// </summary>
+    public sealed class ConnectionStringExtractor
+    {
+
+        /// <summary>
+        /// Connection string as a whole.
+        /// </summary>
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// All extracted key value pairs from the connection string.
+        /// </summary>
+        private readonly Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="connectionString">The connetion string to analyze</param>
+        public ConnectionStringExtractor(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString)) { throw new ArgumentException("Connection string can not be null"); }
+            _connectionString = connectionString;
+            ExtractKeyValuePairs();
+        }
+
+        /// <summary>
+        /// Extracts all the key value pairs from our connection string.
+        /// </summary>
+        private void ExtractKeyValuePairs()
+        {
+            var parts = _connectionString.Split(';');
+            foreach (var part in parts)
+            {
+                var keyAndValue = part.Split('=', 2);
+                var key = keyAndValue[0];
+                var value = keyAndValue[1];
+                keyValuePairs.Add(key, value);
+            }
+        }
+
+        /// <summary>
+        /// Checks if our connection string contains a key.
+        /// </summary>
+        /// <param name="key">The key to check</param>
+        /// <returns>True if the connection string contains the key</returns>
+        public bool HasKey(string key)
+            => keyValuePairs.ContainsKey(key);
+
+        /// <summary>
+        /// Gets a value based on some key.
+        /// </summary>
+        /// <remarks>
+        /// This will throw if the key does not exist.
+        /// </remarks>
+        /// <param name="key">The key to check</param>
+        /// <returns>The corresponding value</returns>
+        public string GetValue(string key)
+            => keyValuePairs[key];
+    }
+}
---
+++ Web/Storage/StorageManager.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Azure.Storage.Blob;
+using Laixer.Library.Configuration.Exceptions;
+using Maximiz.Storage.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage.Auth;
+using Microsoft.Azure.Storage;
+using Microsoft.Extensions.Logging;
+
+namespace Maximiz.Storage
+{
+
+    /// <summary>
+    /// This class manages our storage. 
+    /// TODO Clean up
+    /// </summary>
+    public sealed class StorageManager : IStorageManager
+    {
+
+        private ILogger logger;
+        private readonly string connectionString;
+        private readonly string accountName;
+        private readonly string accountKey;
+        private readonly Uri cdnUri;
+
+        // TODO How to keep track of container?
+        private readonly string containerName = "my-container";
+
+        /// <summary>
+        /// Constructor for dependency injection and connection string setup.
+        /// </summary>
+        /// <param name="configuration"><see cref="IConfiguration"/></param>
+        /// <param name="loggerFactory"><see cref="ILoggerFactory"/></param>
+        public StorageManager(IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            if (loggerFactory == null) { throw new ConfigurationException("Missing logger factory for storage manager"); }
+            logger = loggerFactory.CreateLogger(nameof(StorageManager));
+
+            connectionString = configuration.GetConnectionString("StorageAccountImages");
+            if (string.IsNullOrEmpty(connectionString)) { throw new ConfigurationException("Missing connection string for storage manager"); }
+
+            var cdnUriString = configuration.GetValue<string>("CdnImageUri");
+            if (string.IsNullOrEmpty(cdnUriString)) { throw new ConfigurationException("Missing CDN URI for storage manager"); }
+            cdnUri = new Uri(cdnUriString);
+
+            var extractor = new ConnectionStringExtractor(connectionString);
+            if (!extractor.HasKey("AccountName")) { throw new ConfigurationException("Connection string does not contain account name"); }
+            if (!extractor.HasKey("AccountKey")) { throw new ConfigurationException("Connection string does not contain account key"); }
+            accountName = extractor.GetValue("AccountName");
+            accountKey = extractor.GetValue("AccountKey");
+        }
+
+        /// <summary>
+        /// Attempts to upload a file.
+        /// </summary>
+        /// <remarks>
+        /// If the upload fails, the exception gets caught and logged. The 
+        /// function will return false in this case.
+        /// </remarks>
+        /// <param name="file"></param>
+        /// <returns>True if successful</returns>
+        public async Task<bool> UploadFile(IFormFile file)
+        {
+            try
+            {
+                var storageCredentials = new StorageCredentials(accountName, accountKey);
+                var storageAccount = new CloudStorageAccount(storageCredentials, true);
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(containerName);
+                await container.CreateIfNotExistsAsync();
+
+                await container.SetPermissionsAsync(new BlobContainerPermissions()
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Blob
+                });
+
+                // TODO Should we use a GUID for this?
+                var blob = container.GetBlockBlobReference(Guid.NewGuid().ToString());
+                using (var stream = file.OpenReadStream())
+                {
+                    blob.Properties.ContentType = file.ContentType;
+                    await blob.UploadFromStreamAsync(stream);
+                }
+
+                return true;
+            } catch (Exception e)
+            {
+                logger.LogError("Exception in Storage Manager: ", e);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all uploaded images from the blob storage.
+        /// TODO Clean up
+        /// </summary>
+        /// <param name="query">Search query string</param>
+        /// <returns><see cref="Uri"/> list pointing to CDN images</returns>
+        public async Task<IEnumerable<Uri>> GetUploadedImages(string query = null)
+        {
+            StorageCredentials credentials = new StorageCredentials(accountName, accountKey);
+            CloudStorageAccount storageAccount = new CloudStorageAccount(credentials, true);
+            CloudBlobClient client = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference(containerName);
+            var blobs = await container.ListBlobsSegmentedAsync(new BlobContinuationToken());
+
+            var cdnUris = new List<Uri>();
+            foreach (var blob in blobs.Results)
+            {
+                cdnUris.Add(new Uri(cdnUri, blob.Uri.LocalPath));
+            }
+            return cdnUris;
+        }
+
+    }
+}
