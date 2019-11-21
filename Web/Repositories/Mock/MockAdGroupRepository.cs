12d6b67d7e13d03c1f28b9732574d3f21479b4c0
tabeckers <tabeckers95@gmail.com>
2019-11-21 17:30:02 +0100

    Updated mock repositories

--- Web/Repositories/Mock/MockAdGroupRepository.cs
+++ Web/Repositories/Mock/MockAdGroupRepository.cs
@@ -9,53 +9,57 @@ namespace Maximiz.Repositories.Mock
 {
     public class MockAdGroupRepository : IAdGroupRepository
     {
+        // Initialize random object
+        private Random random = new Random();
+
+        // Re-use the same GUIDS throughout the entire process
+        private static List<Guid> guids = new List<Guid>();
+        static MockAdGroupRepository()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                guids.Add(Guid.NewGuid());
+            }
+        }
+
         public async Task<AdGroupWithStats> Get(Guid id)
-            => AdGroupOne();
+            => AdGroupOne(0);
 
         public async Task<IEnumerable<AdGroupWithStats>> GetAllAsync(QueryAdGroupWithStats query, int page = 0)
-            => new List<AdGroupWithStats> { AdGroupOne(), AdGroupTwo(), AdGroupOne(), AdGroupTwo(), AdGroupTwo() , AdGroupOne(), AdGroupTwo(), AdGroupOne(), AdGroupTwo(), AdGroupTwo() };
+            => GenerateRandom(random.Next(5, 25));
 
         public Task<int> GetCount(QueryAdGroupWithStats query)
-            => Task.FromResult(new Random().Next(23, 1875));
+            => Task.FromResult(random.Next(20, 35));
 
         public async Task<IEnumerable<AdGroupWithStats>> GetLinkedWithCampaignAsync(Guid idCampaign, QueryAdGroupWithStats query)
+            => GenerateRandom(random.Next(2, 5));
+
+        public async Task<IEnumerable<AdGroupWithStats>> GetQueriedAsync(QueryAdGroupWithStats query, int page = 0)
+            => GenerateRandom(random.Next(5, 25));
+
+        private IEnumerable<AdGroupWithStats> GenerateRandom(int total)
         {
-            await Task.Delay(500);
-            return new List<AdGroupWithStats> { AdGroupOne(), AdGroupTwo() };
+            var result = new List<AdGroupWithStats>();
+            if (total > guids.Count) { total = guids.Count; }
+            for (int i = 0; i < total; i++)
+            {
+                result.Add(AdGroupOne(i));
+            }
+            return result;
         }
 
-        public Task<IEnumerable<AdGroupWithStats>> GetQueriedAsync(QueryAdGroupWithStats query, int page = 0)
-        {
-            throw new NotImplementedException();
-        }
-
-        private AdGroupWithStats AdGroupOne()
+        private AdGroupWithStats AdGroupOne(int idIndex)
         {
             return new AdGroupWithStats()
             {
-                Id = Guid.NewGuid(),
-                Name = "AdGroup A",
+                Id = guids[idIndex],
+                Name = "Dummy Ad Group",
                 Description = "A lovely adgroup",
                 ImageLinks = new[] { "link.one", "link.two", "link.three" },
                 Titles = new[] { "Image one", "Image two", "Image three" },
                 CreateDate = DateTime.Now,
                 UpdateDate = DateTime.Now,
-                AdItemCount = new Random().Next(3, 40)
-            };
-        }
-
-        private AdGroupWithStats AdGroupTwo()
-        {
-            return new AdGroupWithStats()
-            {
-                Id = Guid.NewGuid(),
-                Name = "AdGroup B",
-                Description = "A lovely adgroup",
-                ImageLinks = new[] { "link.one", "link.two", "link.three" },
-                Titles = new[] { "Image one", "Image two", "Image three" },
-                CreateDate = DateTime.Now,
-                UpdateDate = DateTime.Now,
-                AdItemCount = new Random().Next(3, 40)
+                AdItemCount = random.Next(3, 40)
             };
         }
     }
--- Web/Repositories/Mock/MockCampaignRepository.cs
+++ Web/Repositories/Mock/MockCampaignRepository.cs
@@ -4,6 +4,7 @@ using Maximiz.Repositories.Abstraction;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Maximiz.Model.Enums;
 
 namespace Maximiz.Repositories.Mock
 {
@@ -13,95 +14,77 @@ namespace Maximiz.Repositories.Mock
     /// </summary>
     public class MockCampaignRepository : ICampaignRepository
     {
+        private Random random = new Random();
 
         public async Task<CampaignWithStats> Get(Guid id) => GetCampaignOne();
 
         public async Task<IEnumerable<CampaignWithStats>> GetAllAsync(QueryCampaignWithStats query, int page = 0)
-        {
-            var result = new List<CampaignWithStats>();
-            result.Add(GetCampaignOne());
-            result.Add(GetCampaignTwo());
-            result.Add(GetCampaignOne());
-            result.Add(GetCampaignTwo());
-            result.Add(GetCampaignOne());
-            result.Add(GetCampaignTwo());
-            return result;
-        }
+            => GenerateRandom(random.Next(4, 25));
 
         public async Task<IEnumerable<CampaignWithStats>> GetActiveAsync(QueryCampaignWithStats query, int page = 0)
-            => new List<CampaignWithStats>
-            {
-                GetCampaignOne(),
-                GetCampaignOne()
-            };
+            => GenerateRandom(random.Next(4, 25));
 
         public async Task<IEnumerable<CampaignWithStats>> GetInactiveAsync(QueryCampaignWithStats query, int page = 0)
-        {
-            var result = new List<CampaignWithStats>();
-            result.Add(GetCampaignTwo());
-            return result;
-        }
+            => GenerateRandom(random.Next(4, 25));
 
         public async Task<IEnumerable<CampaignWithStats>> GetPendingAsync(QueryCampaignWithStats query, int page = 0)
+            => GenerateRandom(random.Next(4, 25));
+
+        public async Task<IEnumerable<CampaignWithStats>> GetQueriedAsync(QueryCampaignWithStats query, int page = 0)
+            => GenerateRandom(random.Next(4, 25));
+
+        public Task<int> GetCount(QueryCampaignWithStats query)
+            => Task.FromResult(new Random().Next(40, 350));
+
+        private IEnumerable<CampaignWithStats> GenerateRandom(int total)
         {
             var result = new List<CampaignWithStats>();
-            result.Add(GetCampaignTwo());
-            result.Add(GetCampaignTwo());
-            result.Add(GetCampaignTwo());
+            for (int i = 0; i < total; i++)
+            {
+                result.Add(GetCampaignOne());
+            }
             return result;
-        }
-
-        public Task<IEnumerable<CampaignWithStats>> GetQueriedAsync(QueryCampaignWithStats query, int page = 0)
-        {
-            throw new NotImplementedException();
         }
 
         private CampaignWithStats GetCampaignOne() => new CampaignWithStats
         {
+            // Main
             Id = Guid.NewGuid(),
-            Name = "Dummy campaign one",
+
+            // Stats
+            Spent = random.Next(100, 1000),
+            Clicks = random.Next(10000),
+            Roi = random.NextDouble()*100,
+            Revenue = random.Next(0, 10000),
+            RevenueAdsense = random.Next(0, 5000),
+            RevenueTaboola = random.Next(0, 5000),
+            Profit = random.Next(0, 3000),
+            Actions = random.Next(10000),
+
+            // Account
+            Publisher = Publisher.Taboola,
+            Name = "Dummy campaign",
             BrandingText = "Some branding text",
-            Budget = 54,
-            Spent = 1000,
-            Clicks = 18548,
-            Roi = 66.49,
-            Revenue = 1337,
-            RevenueAdsense = 1,
-            RevenueTaboola = 1336,
-            Profit = 337,
-            Actions = 1574,
+            Utm = "base_utm_code",
+            TargetUrl = "https://www.laixer.com",
+
+            // Marketing
+            Budget = random.Next(30, 3000),
+            BudgetDaily = random.Next(0, 100),
+            BidStrategy = BidStrategy.Smart,
+            Delivery = Delivery.Balanced,
+            InitialCpc = (decimal)random.NextDouble()*5,
+
+            // Targeting
+            Devices = new[] { Device.Desktop },
+            OperatingSystems = new[] { OS.Android },
+            LocationInclude = new[] { 1 },
+
+            // Schedule
             StartDate = DateTime.Now,
             EndDate = DateTime.Now,
-            Publisher = Model.Enums.Publisher.Taboola,
-            TargetUrl = "www.oznak.nl",
-            Utm = "base_utm_code",
-            Delivery = Model.Enums.Delivery.Balanced
         };
 
-        private CampaignWithStats GetCampaignTwo() => new CampaignWithStats
-        {
-            Id = Guid.NewGuid(),
-            Name = "Dummy campaign two",
-            BrandingText = "Some branding text",
-            Budget = 185,
-            Spent = 8000,
-            Clicks = 183748,
-            Roi = 32.45,
-            Revenue = 16657,
-            RevenueAdsense = 10000,
-            RevenueTaboola = 6657,
-            Profit = 8657,
-            Actions = 1574,
-            StartDate = DateTime.Now,
-            EndDate = DateTime.Now,
-            Publisher = Model.Enums.Publisher.Taboola,
-            TargetUrl = "www.oznak.nl",
-            Utm = "base_utm_code",
-            Delivery = Model.Enums.Delivery.Strict
-        };
-
-        public Task<int> GetCount(QueryCampaignWithStats query)
-            => Task.FromResult(new Random().Next(40, 350));
     }
 
 }
