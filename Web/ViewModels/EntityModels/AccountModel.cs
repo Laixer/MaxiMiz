4d566a949932afbb00e58e1a64c5ff08d000d2b9
tabeckers <tabeckers95@gmail.com>
2019-12-02 10:37:36 +0100

    Updated viewmodels

--- Web/ViewModels/EntityModels/AccountModel.cs
+++ Web/ViewModels/EntityModels/AccountModel.cs
@@ -20,5 +20,17 @@ namespace Maximiz.ViewModels.EntityModels
         /// </summary>
         public Publisher Publisher { get; set; } 
 
+        /// <summary>
+        /// Contains the type of account.
+        /// 
+        /// TODO This seems rather unsafe! A lot to do for our mapper.
+        /// The reason I choose this structure is because the database object
+        /// layout might change. At the moment our desired 'string' is in the 
+        /// details json field of our database. We want to extract the field
+        /// and parse it during mapping.
+        /// </summary>
+        public string AccountTypeString { get; set; }
+
     }
+
 }
---
+++ Web/ViewModels/Settings/LinkedAccountsTableViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Maximiz.ViewModels.EntityModels;
+using System.Collections.Generic;
+
+namespace Maximiz.ViewModels.Settings
+{
+
+    /// <summary>
+    /// Viewmodel for displaying all linked accounts.
+    /// </summary>
+    public sealed class LinkedAccountsTableViewModel
+    {
+
+        /// <summary>
+        /// List of linked accounts we wish to display.
+        /// </summary>
+        public IEnumerable<AccountModel> LinkedAccounts { get; set; }
+
+    }
+}
