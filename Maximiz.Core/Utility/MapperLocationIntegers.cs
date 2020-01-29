8b0f498ea94977db2c4eb7416239e37f143642ec
tabeckers <tabeckers95@gmail.com>
2020-01-29 14:32:55 +0100

    Created utility functionality for core
    Some mapping, some expanding of campaign groups, utm name generators, campaign name generators, etc

---
+++ Maximiz.Core/Utility/MapperLocationIntegers.cs
@@ -0,0 +1,70 @@
+﻿using Maximiz.Model.Enums;
+using System;
+using System.Linq;
+
+namespace Maximiz.Core.Utility
+{
+
+    /// <summary>
+    /// Contains functionality to map from <see cref="Location"/> to the 
+    /// corresponding integer value.
+    /// </summary>
+    public static class MapperLocationIntegers
+    {
+
+        /// <summary>
+        /// Map from <see cref="Location"/> to the corresponding integer value.
+        /// </summary>
+        /// <param name="location"><see cref="Location"/></param>
+        /// <returns>Corresponding integer value</returns>
+        public static int Map(Location location)
+        {
+            switch (location)
+            {
+                case Location.NL:
+                    return 0;
+                case Location.UK:
+                    return 1;
+                case Location.ES:
+                    return 2;
+                case Location.DE:
+                    return 3;
+                case Location.FR:
+                    return 4;
+            }
+
+            throw new InvalidOperationException(nameof(location));
+        }
+
+        /// <summary>
+        /// Map from location integer value to the corresponding <see cref="Location"/>.
+        /// </summary>
+        /// <param name="locationInteger">Location integer value</param>
+        /// <returns><see cref="Location"/></returns>
+        public static Location Map(int locationInteger)
+        {
+            switch (locationInteger)
+            {
+                case 0:
+                    return Location.NL;
+                case 1:
+                    return Location.UK;
+                case 2:
+                    return Location.FR;
+                case 3:
+                    return Location.DE;
+                case 4:
+                    return Location.FR;
+            }
+
+            throw new InvalidOperationException(nameof(locationInteger));
+        }
+
+        public static int[] MapMultiple(Location[] locations)
+            => locations.Select(x => Map(x)).ToArray();
+
+        public static Location[] MapMultiple(int[] locationIntegers)
+            => locationIntegers.Select(x => Map(x)).ToArray();
+
+    }
+}
